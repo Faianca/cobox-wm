eb69da80b79c1610587623b46360e3d93be326e7
Jorge Faianca <jorgefaianca@gmail.com>
2015-03-17 20:23:28 +0000

    start up

--- main.d
+++ main.d
@@ -3,5 +3,37 @@ import cbox;
 
 void main()
 {
+	const VERSION = 0.1;
+
 	auto base = new App();
+
+	if(args.length == 2 && args[1] == "-v") {
+		stderr.writeln("cbox-wm-"~VERSION~"\n"~
+			"The Cteam Window Manager.\n\t"~
+			"© 2015 Cbox, see LICENSE for details\n\t"~
+			"© 2014-2015 cbox engineers, see LICENSE for details");
+		return -1;
+
+	} else if(args.length != 1) {
+		stderr.writeln("usage: cbox-wm [-v]");
+		return -1;
+	}
+	
+	if(!setlocale(LC_CTYPE, "".toStringz) || !XSupportsLocale()) {
+		stderr.writeln("warning: no locale support");
+		return -1;
+	}
+	dpy = XOpenDisplay(null);
+	if(dpy is null) {
+		stderr.writeln("cbox: cannot open display");
+		return -1;
+	}
+	checkotherwm();
+	setup();
+	scan();
+	run();
+	cleanup();
+	XCloseDisplay(dpy);
+	lout("cbox-"~VERSION~" end");
+	return 0;
 }
---
+++ monitor.d
@@ -0,0 +1,40 @@
+﻿module monitor;
+
+class Monitor
+{
+	void arrange(Monitor *m) 
+	{
+		if(m) {
+			showhide(m.stack);
+		} else foreach(m; mons.range) {
+			showhide(m.stack);
+		}
+		if(m) {
+			arrangemon(m);
+			restack(m);
+		} else foreach(m; mons.range) {
+			arrangemon(m);
+		}
+	}
+	
+	void arrangemon(Monitor *m) 
+	{
+		m.ltsymbol = m.lt[m.sellt].symbol;
+		
+		if(m.lt[m.sellt].arrange)
+			m.lt[m.sellt].arrange(m);
+	}
+
+	void attach(Client *c) 
+	{
+		c.next = c.mon.clients;
+		c.mon.clients = c;
+	}
+	
+	void attachstack(Client *c) 
+	{
+		c.snext = c.mon.stack;
+		c.mon.stack = c;
+	}
+
+}
